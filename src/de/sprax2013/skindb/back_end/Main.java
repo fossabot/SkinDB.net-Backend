0671b6b482da14414d72e700f023aafd179f6278
Sprax2013 <sprax2013@gmail.com>
2019-01-16 07:13:10 +0100

    Create Main.java

---
+++ src/de/sprax2013/skindb/back_end/Main.java
@@ -0,0 +1,12 @@
+package de.sprax2013.skindb.back_end;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(" _____  _     _        ______ ______");
+		System.out.println("/  ___|| |   (_)       |  _  \\| ___ \\");
+		System.out.println("\\ `--. | | __ _  _ __  | | | || |_/ /");
+		System.out.println(" `--. \\| |/ /| || '_ \\ | | | || ___ \\");
+		System.out.println("/\\__/ /|   < | || | | || |/ / | |_/ /");
+		System.out.println("\\____/ |_|\\_\\|_||_| |_||___/  \\____/");
+	}
+}
